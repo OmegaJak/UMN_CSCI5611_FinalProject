7433ae5d9162ae9da55bd9bb788c7e4c5cf4decf
hao00022 <hao00022@umn.edu>
2019-05-08 22:10:37 -0500

    format raytracer before add walls

--- src/RayTracer.cpp
+++ src/RayTracer.cpp
@@ -1,9 +1,9 @@
 #include "RayTracer.h"
 #include <SoundManager.h>
 #include <cmath>
+#include <iostream>
 #include "Camera.h"
 #include "minmax.h"
-#include <iostream>
 extern float timePassed;
 glm::vec3 RayTracer::listenerPoint;
 void RayTracer::shootThemAll(int index) {
@@ -11,7 +11,7 @@ void RayTracer::shootThemAll(int index) {
     const glm::vec3 sourcPoint = glm::vec3(0, 0, 0);
 
     listenerPoint = Camera::getInstance().GetPosition();
-    listenerPoint.z = 0; // de Dimesionalize
+    listenerPoint.z = 0;  // de Dimesionalize
 
     const float AngleStep = 2 * M_PI / RayNumber;
     float angle = 0;
@@ -24,29 +24,29 @@ void RayTracer::shootThemAll(int index) {
 
 float RayTracer::Ray::hitListenr() {
     float projecLength = glm::dot(_dir, (listenerPoint - _start));
-	if(projecLength <= 0|| projecLength > MaxListenDis) {
-		//prune useless calculation
-    } else if (glm::distance(_start + projecLength * _dir, listenerPoint) <= ListenerRadius) 
-		return projecLength;
-    
+    if (projecLength <= 0 || projecLength > MaxListenDis) {
+        // prune useless calculation
+    } else if (glm::distance(_start + projecLength * _dir, listenerPoint) <= ListenerRadius)
+        return projecLength;
+
     return MaxListenDis;
 }
 
 float RayTracer::Ray::hitWal(float listenerDis, int step) {
     return MaxListenDis;
-	// if Hit wall recursive raytrace here
+    // if Hit wall recursive raytrace here
 }
 
-void RayTracer::Ray::trace(int index, int step) {
+void RayTracer::Ray::trace(int index, int step) {	
     float lDis = hitListenr();
-    float wDis = hitWal(lDis,step);
-    
+    float wDis = hitWal(lDis, step);
+
     if (lDis < wDis) {
-        //add current to buffer
+        // add current to buffer
         SoundManager::addBuffer(timePassed, index);
-		SoundManager::addBuffer(timePassed+0.2, index);
-		//SoundManager::addBuffer(timePassed+0.8, index);
-		//SoundManager::addBuffer(timePassed+0.4, index);
-		//SoundManager::addBuffer(timePassed+0.6, index);
+        //SoundManager::addBuffer(timePassed + 0.2, index);
+        // SoundManager::addBuffer(timePassed+0.8, index);
+        // SoundManager::addBuffer(timePassed+0.4, index);
+        // SoundManager::addBuffer(timePassed+0.6, index);
     }
 }
--- src/RayTracer.h
+++ src/RayTracer.h
@@ -1,26 +1,28 @@
 #pragma once
 #include <glm/vec3.hpp>
 class RayTracer {
-public:
-	enum {
-		RayNumber = 1024,
-		ListenerRadius = 10,
-		// use one of them to stop raytrace
-		MaxReflection = 5,
-		MaxListenDis = 420,
-	};
-private:
+   public:
+    enum {
+        RayNumber = 1024,
+        ListenerRadius = 10,
+        // use one of them to stop raytrace
+        MaxReflection = 5,
+        MaxListenDis = 420,
+		SoundSpeed = 340,
+    };
+
+   private:
     RayTracer() {}
     class Ray {
         glm::vec3 _start;
         glm::vec3 _dir;
 
        public:
-        Ray(glm::vec3 start, glm::vec3 dir) : _start(start), _dir(dir){}
+        Ray(glm::vec3 start, glm::vec3 dir) : _start(start), _dir(dir) {}
         void trace(int index, int step = MaxReflection);
         float hitListenr();
-		float hitWal(float listenerDis, int step);
-	};
+        float hitWal(float listenerDis, int step);
+    };
     static glm::vec3 listenerPoint;
 
    public:
