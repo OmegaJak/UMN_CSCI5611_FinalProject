22c429860078dee639aebebd6170ea8b703baefc
OmegaJak <jak.kruger@gmail.com>
2019-05-12 19:28:57 -0500

    Add floor and ceiling

--- src/Environment.cpp
+++ src/Environment.cpp
@@ -31,58 +31,80 @@ void Environment::CreateEnvironment() {
     // gameObject.material_.specFactor_ = 0.2;
     //_gameObjects.push_back(gameObject);
 
-	/*
-    gameObject = GameObject(_cubeModel);  // reference person
-    gameObject.SetTextureIndex(TEX1);
-    gameObject.SetScale(1, 0, -3);
-    gameObject.SetPosition(glm::vec3(-25, 13, 1.5));
+    /*
+gameObject = GameObject(_cubeModel);  // reference person
+gameObject.SetTextureIndex(TEX1);
+gameObject.SetScale(1, 0, -3);
+gameObject.SetPosition(glm::vec3(-25, 13, 1.5));
+_gameObjects.push_back(gameObject);
+    */
+    const float WallScale = 42;
+    // end walls near strings
+    _walls.push_back(Wall(glm::vec3(-2 * WallScale, 0, 0), glm::vec3(1, 0, 0), 2 * WallScale, _cubeModel));
+    _walls.push_back(Wall(glm::vec3(-2 * WallScale, 2 * WallScale, 0), glm::vec3(1, 0, 0), 2 * WallScale, _cubeModel));
+    _walls.push_back(Wall(glm::vec3(-2 * WallScale, -2 * WallScale, 0), glm::vec3(1, 0, 0), 2 * WallScale, _cubeModel));
+
+    // long side walls
+    _walls.push_back(Wall(glm::vec3(-WallScale, 3 * WallScale, 0), glm::vec3(0, -1, 0), 2 * WallScale, _cubeModel));
+    _walls.push_back(Wall(glm::vec3(-WallScale, -3 * WallScale, 0), glm::vec3(0, -1, 0), 2 * WallScale, _cubeModel));
+    _walls.push_back(Wall(glm::vec3(WallScale, 3 * WallScale, 0), glm::vec3(0, -1, 0), 2 * WallScale, _cubeModel));
+    _walls.push_back(Wall(glm::vec3(WallScale, -3 * WallScale, 0), glm::vec3(0, -1, 0), 2 * WallScale, _cubeModel));
+    _walls.push_back(Wall(glm::vec3(3 * WallScale, 3 * WallScale, 0), glm::vec3(0, -1, 0), 2 * WallScale, _cubeModel));
+    _walls.push_back(Wall(glm::vec3(3 * WallScale, -3 * WallScale, 0), glm::vec3(0, -1, 0), 2 * WallScale, _cubeModel));
+
+    // doorway walls
+    _walls.push_back(Wall(glm::vec3(4 * WallScale, 2 * WallScale, 0), glm::vec3(1, 0, 0), 2 * WallScale, _cubeModel));
+    _walls.push_back(Wall(glm::vec3(4 * WallScale, -2 * WallScale, 0), glm::vec3(1, 0, 0), 2 * WallScale, _cubeModel));
+
+    // side room side walls
+    _walls.push_back(Wall(glm::vec3(5 * WallScale, 3 * WallScale, 0), glm::vec3(0, -1, 0), 2 * WallScale, _cubeModel));
+    _walls.push_back(Wall(glm::vec3(5 * WallScale, -3 * WallScale, 0), glm::vec3(0, -1, 0), 2 * WallScale, _cubeModel));
+
+    // side room end walls
+    _walls.push_back(Wall(glm::vec3(6 * WallScale, 0, 0), glm::vec3(1, 0, 0), 2 * WallScale, _cubeModel));
+    _walls.push_back(Wall(glm::vec3(6 * WallScale, 2 * WallScale, 0), glm::vec3(1, 0, 0), 2 * WallScale, _cubeModel));
+    _walls.push_back(Wall(glm::vec3(6 * WallScale, -2 * WallScale, 0), glm::vec3(1, 0, 0), 2 * WallScale, _cubeModel));
+
+    /*
+    _walls.push_back(Wall(glm::vec3(-WallScale,WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
+    _walls.push_back(Wall(glm::vec3(-WallScale,-WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
+    _walls.push_back(Wall(glm::vec3(-WallScale,-3*WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
+    _walls.push_back(Wall(glm::vec3(-WallScale,3*WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
+    //_walls.push_back(Wall(glm::vec3(WallScale,0,0), glm::vec3(-1,0,0), 2*WallScale,_cubeModel));
+    _walls.push_back(Wall(glm::vec3(0,4*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
+    _walls.push_back(Wall(glm::vec3(0,-4*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
+    _walls.push_back(Wall(glm::vec3(2*WallScale,4*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
+    _walls.push_back(Wall(glm::vec3(2*WallScale,-4*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
+
+    _walls.push_back(Wall(glm::vec3(3*WallScale,3*WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
+    _walls.push_back(Wall(glm::vec3(3*WallScale,-3*WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
+    _walls.push_back(Wall(glm::vec3(3*WallScale,-WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
+    */
+    //_walls.push_back(Wall(glm::vec3(0,-WallScale,0), glm::vec3(0,1,0), 2*WallScale,_cubeModel));
+    //_walls.push_back(Wall(glm::vec3(2*WallScale,WallScale,0), glm::vec3(0,1,0), 2*WallScale,_cubeModel));
+    GameObject gameObject;
+    // floor
+    gameObject = GameObject(_cubeModel);
+    gameObject.SetTextureIndex(UNTEXTURED);
+    gameObject.SetColor(glm::vec3(1, 1, 1));
+    gameObject.SetScale(12 * WallScale, 6 * WallScale, 1);
+    gameObject.SetPosition(glm::vec3(0, 0, -WallScale));
     _gameObjects.push_back(gameObject);
-	*/
-	const float WallScale = 42;
-	_walls.push_back(Wall(glm::vec3(-2*WallScale,0,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(-2*WallScale,2*WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(-2*WallScale,-2*WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
 
-	_walls.push_back(Wall(glm::vec3(-WallScale,3*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(-WallScale,-3*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(WallScale,3*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(WallScale,-3*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(3*WallScale,3*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(3*WallScale,-3*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
+    // ceiling
+    gameObject = GameObject(_cubeModel);
+    gameObject.SetTextureIndex(UNTEXTURED);
+    gameObject.SetColor(glm::vec3(1, 1, 1));
+    gameObject.SetScale(12 * WallScale, 6 * WallScale, 1);
+    gameObject.SetPosition(glm::vec3(0, 0, WallScale));
+    _gameObjects.push_back(gameObject);
 
-	_walls.push_back(Wall(glm::vec3(4*WallScale,2*WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(4*WallScale,-2*WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
-
-	_walls.push_back(Wall(glm::vec3(5*WallScale,3*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(5*WallScale,-3*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
-
-	_walls.push_back(Wall(glm::vec3(6*WallScale,0,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(6*WallScale,2*WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(6*WallScale,-2*WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
-
-	/*
-	_walls.push_back(Wall(glm::vec3(-WallScale,WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(-WallScale,-WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(-WallScale,-3*WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(-WallScale,3*WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
-	//_walls.push_back(Wall(glm::vec3(WallScale,0,0), glm::vec3(-1,0,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(0,4*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(0,-4*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(2*WallScale,4*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(2*WallScale,-4*WallScale,0), glm::vec3(0,-1,0), 2*WallScale,_cubeModel));
-
-	_walls.push_back(Wall(glm::vec3(3*WallScale,3*WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(3*WallScale,-3*WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
-	_walls.push_back(Wall(glm::vec3(3*WallScale,-WallScale,0), glm::vec3(1,0,0), 2*WallScale,_cubeModel));
-	*/
-	//_walls.push_back(Wall(glm::vec3(0,-WallScale,0), glm::vec3(0,1,0), 2*WallScale,_cubeModel));
-	//_walls.push_back(Wall(glm::vec3(2*WallScale,WallScale,0), glm::vec3(0,1,0), 2*WallScale,_cubeModel));
-	GameObject gameObject;
     gameObject = GameObject(_sphereModel);
     gameObject.SetTextureIndex(UNTEXTURED);
     gameObject.SetColor(glm::vec3(0, 0, 0));
     gameObject.SetPosition(glm::vec3(1000, 1000, 1000));
-    gameObject.SetScale(8, 8, 8);
+    gameObject.SetScale(1, 1, 1);
     _gameObjects.push_back(gameObject);
+
     _gravityCenterIndex = _gameObjects.size() - 1;
 }
--- src/Environment.h
+++ src/Environment.h
@@ -3,38 +3,39 @@
 
 class Environment {
    public:
-    
-	static Environment& getInstance() {
+    static Environment& getInstance() {
         static Environment instance;
         return instance;
     }
     void UpdateAll();
     void SetGravityCenterPosition(const glm::vec3& position);
-	struct Wall : public GameObject{
-		glm::vec3 _position;
-		glm::vec3 _normal;
-		float _length;
-		Wall(glm::vec3 pos, glm::vec3 n, float l, Model* model):GameObject(model), _position(pos), _normal(n), _length(l){
-			SetTextureIndex(TEX1);
-		    SetColor(glm::vec3(0, 1, 0));
-			SetPosition(_position);
-			glm::normalize(_normal);
-			// TODO: rotation doesn't work...
-			//EulerRotate(45,.0f,.0f);
+    struct Wall : public GameObject {
+        glm::vec3 _position;
+        glm::vec3 _normal;
+        float _length;
+        Wall(glm::vec3 pos, glm::vec3 n, float l, Model* model) : GameObject(model), _position(pos), _normal(n), _length(l) {
+            SetTextureIndex(TEX1);
+            SetColor(glm::vec3(0, 1, 0));
+            SetPosition(_position);
+            glm::normalize(_normal);
+            // TODO: rotation doesn't work...
+            // EulerRotate(45,.0f,.0f);
 
-			SetScale(-l*_normal.y+0.1f, -l* _normal.x+0.1f, l);
-		}
-		Wall() = default;
+            SetScale(-l * _normal.y + 0.1f, -l * _normal.x + 0.1f, l);
+        }
+        Wall() = default;
+    };
 
-	};
+    const std::vector<Wall>& getWalls() {
+        return _walls;
+    }
 
-	const std::vector<Wall>& getWalls(){return _walls;}
    private:
-	Environment();
+    Environment();
     void CreateEnvironment();
 
     std::vector<GameObject> _gameObjects;
-	std::vector<Wall> _walls;// TODO: try to unify these two
+    std::vector<Wall> _walls;  // TODO: try to unify these two
     int _gravityCenterIndex;
     Model* _cubeModel;
     Model* _sphereModel;
